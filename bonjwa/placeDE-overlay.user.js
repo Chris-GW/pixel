02c6cffe21ba8a14721ef5ea66f832c2646e0683
Chris-GW <Chris-GW@users.noreply.github.com>
2023-07-21 08:48:50 +0200

    v5 mit allen Authoren und verschoben in Projekt Root Verzeichnis /placeDE-overlay.user.js

--- bonjwa/placeDE-overlay.user.js
+++ bonjwa/placeDE-overlay.user.js
@@ -1,21 +1,23 @@
 // ==UserScript==
 // @name         Bonjwa r/placeDE Overlay
 // @namespace    http://tampermonkey.net/
-// @version      4
+// @version      5
 // @description  try to take over the canvas!
-// @author       Chris-GW
+// @author       Chris-GW, nama17, mp1404
 // @match        https://garlic-bread.reddit.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/Chris-GW/pixel/raw/main/bonjwa/placeDE-overlay.user.js
-// @downloadURL  https://github.com/Chris-GW/pixel/raw/main/bonjwa/placeDE-overlay.user.js
+// @updateURL    https://github.com/Chris-GW/pixel/raw/main/placeDE-overlay.user.js
+// @downloadURL  https://github.com/Chris-GW/pixel/raw/main/placeDE-overlay.user.js
 // ==/UserScript==
 let url = "https://raw.githubusercontent.com/Chris-GW/pixel/main/overlay.png"
 
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
         document.getElementsByTagName("garlic-bread-embed")[0]
-        .shadowRoot.children[0].children[0]
-        .getElementsByTagName("garlic-bread-camera")[0].children[0]
+        .shadowRoot.children[0]
+        .children[0]
+        .getElementsByTagName("garlic-bread-camera")[0]
+        .children[0]
         .shadowRoot.children[0]
         .appendChild(
             (function () {
--- placeDE-overlay.user.js
+++ placeDE-overlay.user.js
@@ -1,20 +1,25 @@
 // ==UserScript==
-// @name         r/placeDE Template
+// @name         Bonjwa r/placeDE Overlay
 // @namespace    http://tampermonkey.net/
-// @version      4
+// @version      5
 // @description  try to take over the canvas!
-// @author       Chris-GW, nama17
+// @author       Chris-GW, nama17, mp1404
 // @match        https://garlic-bread.reddit.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/nama17/pixel/raw/main/overlay.user.js
-// @downloadURL  https://github.com/nama17/pixel/raw/main/overlay.user.js
+// @updateURL    https://github.com/Chris-GW/pixel/raw/main/placeDE-overlay.user.js
+// @downloadURL  https://github.com/Chris-GW/pixel/raw/main/placeDE-overlay.user.js
 // ==/UserScript==
-let url = "https://raw.githubusercontent.com/nama17/pixel/main/overlay.png"
+let url = "https://raw.githubusercontent.com/Chris-GW/pixel/main/overlay.png"
 
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
-        console.log("asgfasgsdahdfashgdfhgdfsg", document.getElementsByTagName("garlic-bread-embed"));
-        document.getElementsByTagName("garlic-bread-embed")[0].shadowRoot.children[0].children[0].getElementsByTagName("garlic-bread-camera")[0].children[0].shadowRoot.children[0].appendChild(
+        document.getElementsByTagName("garlic-bread-embed")[0]
+        .shadowRoot.children[0]
+        .children[0]
+        .getElementsByTagName("garlic-bread-camera")[0]
+        .children[0]
+        .shadowRoot.children[0]
+        .appendChild(
             (function () {
                 const i = document.createElement("img");
                 i.src = url;
